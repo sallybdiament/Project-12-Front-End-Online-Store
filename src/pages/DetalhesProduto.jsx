0cdc03d3e15498e3bceded920aaa8706ca0bb583
Sally Diament <sally.barzilai@gmail.com>
2022-07-08 22:45:11 -0300

    incluindo o requisito 07

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,30 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// import Card from '../components/Card';
+import PropTypes from 'prop-types';
+import { getProductsFromProductId } from '../services/api';
 
 class DetalhesProduto extends React.Component {
+  state = {
+    titleProduct: '',
+    priceProduct: '',
+    imgProduct: '',
+    soldQuantityProduct: '',
+  }
+
+  componentDidMount = async () => {
+    const { match } = this.props;
+    const productId = match.params.id;
+    const product = await getProductsFromProductId(productId);
+    console.log(product);
+    this.setState({
+      titleProduct: product.title,
+      priceProduct: product.price,
+      imgProduct: product.thumbnail,
+      soldQuantityProduct: product.sold_quantity,
+    });
+  }
+
   render() {
+    const { titleProduct, priceProduct, imgProduct, soldQuantityProduct } = this.state;
+    // console.log(infoProduto);
     return (
       <div>
-        <p data-testid="product-detail-name">nome</p>
+        <p data-testid="product-detail-name">{titleProduct}</p>
+        <p>{priceProduct}</p>
+        <img src={ imgProduct } alt={ titleProduct } />
+        <p>
+          Quantidade vendida até o momento:
+          {soldQuantityProduct}
+        </p>
         <Link to="/">Voltar</Link>
       </div>
     );
   }
 }
 
-// DetalhesProduto.propTypes = {
-// }.isRequired;
-// DetalhesProduto.propTypes = {
-//   nome: PropTypes.string,
-//   imagem: PropTypes.string,
-//   preco: PropTypes.number,
-//   produtoId: PropTypes.string,
-//   match: PropTypes.objectOf(PropTypes.any),
-//   objeto: PropTypes.shape({
-//     produtoId: PropTypes.string,
-//   }),
-// }.isRequired;
+DetalhesProduto.propTypes = {
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default DetalhesProduto;
