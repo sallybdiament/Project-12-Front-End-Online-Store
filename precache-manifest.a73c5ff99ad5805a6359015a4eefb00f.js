9d958c1f4571a214f73c557d99d51d8f75411ce3
Sally Diament <sally.barzilai@gmail.com>
2022-11-16 21:13:00 -0300

    Updates

---
+++ precache-manifest.a73c5ff99ad5805a6359015a4eefb00f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68964435c71420a9385b1c03e5a0e67d",
+    "url": "/Project-12-Front-End-Online-Store/index.html"
+  },
+  {
+    "revision": "6006f2cc61fb6329400b",
+    "url": "/Project-12-Front-End-Online-Store/static/css/main.302947f6.chunk.css"
+  },
+  {
+    "revision": "e21d03b6a3689c2331db",
+    "url": "/Project-12-Front-End-Online-Store/static/js/2.c862a502.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Project-12-Front-End-Online-Store/static/js/2.c862a502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6006f2cc61fb6329400b",
+    "url": "/Project-12-Front-End-Online-Store/static/js/main.d3fd467f.chunk.js"
+  },
+  {
+    "revision": "71899830ee4dc26f6f9c",
+    "url": "/Project-12-Front-End-Online-Store/static/js/runtime-main.a9413903.js"
+  }
+]);
