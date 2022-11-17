e96a5b6d99573153ed5bd0669ef1d2c72fa0480b
Sally Diament <sally.barzilai@gmail.com>
2022-11-16 21:13:27 -0300

    incluindo os scripts de deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import CarrinhoDeCompras from './pages/CarrinhoDeCompras';
@@ -9,14 +9,14 @@ import Checkout from './pages/Checkout';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/CarrinhoDeCompras/" component={ CarrinhoDeCompras } />
           <Route path="/DetalhesProduto/:id" component={ DetalhesProduto } />
           <Route path="/Checkout/" component={ Checkout } />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
